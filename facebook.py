cc1f29659f5fd0ac10307f46e8d484fa2948c71c
Czarek Grądys <czarekgr@wa.onet.pl>
2019-12-22 16:01:15 +0100

    Formatowanie wyniku

--- facebook.py
+++ facebook.py
@@ -4,67 +4,60 @@ import json
 import datetime
 
 
-
 def marginesy(l, margines):
-    k="\n" + margines
+    k = "\n" + margines
     l = k.join(l.split("\n"))
-    return(l)
+    return (l)
 
 
+for i in range(29, 0, -1):
+    file = 'message_' + str(i) + '.json'
 
-with open('message_1.json' ) as json_file:
-    data = json.load(json_file)
+    with open(file) as json_file:
+        data = json.load(json_file)
 
+        sender_time_old = 'dupa'
+        for p in reversed(data['messages']):
 
-    sender_time_old = 'dupa'
-    for p in reversed(data['messages']):
+            sender_name = p["sender_name"].encode("latin").decode("utf")
+            if sender_name == 'Cezary Grądys':
+                margines = ''
+            else:
+                margines = '\t'
 
-        sender_name = p["sender_name"].encode("latin").decode("utf")
-        if sender_name == 'Cezary Grądys':
-            margines = '      '
-        else:
-            margines = ''
+            time = datetime.datetime.fromtimestamp(p["timestamp_ms"] / 1000.0).strftime("%Y-%m-%d %H:%m")
+            sender_time = sender_name + '  ' + time
 
+            if sender_time_old != sender_time:
+                print("\n" + margines + sender_time)
+                sender_time_old = sender_time
 
-        time = datetime.datetime.fromtimestamp(p["timestamp_ms"]/1000.0).strftime("%Y-%m-%d %H:%m")
-        sender_time = sender_name + '  ' + time
+            if 'content' in p:
+                print(margines + marginesy(p['content'].encode("latin").decode("utf"), margines))
 
+            if 'gifs' in p:
+                print(margines, end='')
+                print(p['gifs'])
 
-        if sender_time_old != sender_time:
-            print("\n" + margines + sender_time)
-            sender_time_old=sender_time
+            if 'photos' in p:
+                print(margines, end='')
+                print(p['photos'])
 
+            if 'sticker' in p:
+                print(margines, end='')
+                print(p['sticker'])
 
-        if 'content' in p:
-            print(margines + marginesy(p['content'].encode("latin").decode("utf"),margines)) # podzielić \n i odwrócić! dla tac!
+            if 'videos' in p:
+                print(margines, end='')
+                print(p['videos'])
 
-        if 'gifs' in p:
-            print(margines, end='')
-            print(p['gifs'])
+            if 'share' in p:
+                print(margines, end='')
+                print(p['share'])
 
-        if 'photos' in p:
-            print(margines, end='')
-            print(p['photos'])
-
-        if 'sticker' in p:
-            print(margines, end='')
-            print( p['sticker'])
-
-        if 'videos' in p:
-            print(margines, end='')
-            print(p['videos'])
-
-        if 'share' in p:
-            print(margines, end='')
-            print(p['share'])
-
-        if 'reactions' in p:
-            print(margines, end='')
-            print(p['reactions'])
-
-
-
-
+            if 'reactions' in p:
+                print(margines, end='')
+                print(p['reactions'])
 
 ''' sender_name timestamp_ms content photos type
 sender_name timestamp_ms content reactions type
